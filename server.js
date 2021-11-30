1e7b677fadea2221a6eabe30649fc1aa64fbe24a
Rob Hess <hessro@oregonstate.edu>
2021-11-30 15:07:16 -0800

    Add code from 11/30 lecture.

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var exphbs = require('express-handlebars');
+var fs = require('fs')
 
 var peopleData = require('./peopleData');
 
@@ -8,6 +9,8 @@ var port = process.env.PORT || 8000;
 
 app.engine('handlebars', exphbs.engine({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
+
+app.use(express.json())
 
 app.use(express.static('public'));
 
@@ -29,6 +32,44 @@ app.get('/people/:person', function (req, res, next) {
     next();
   }
 });
+
+app.post('/people/:person/addPhoto', function (req, res, next) {
+  console.log("  -- req.body:", req.body)
+
+  var url = req.body.url
+  var caption = req.body.caption
+  var person = req.params.person.toLowerCase()
+  console.log("  -- url:", url)
+  console.log("  -- caption:", caption)
+  console.log("  -- person:", person)
+
+  if (peopleData[person]) {
+    if (url && caption) {
+      peopleData[person].photos.push({
+        url: url,
+        caption: caption
+      })
+      console.log("  -- peopleData[" + person + "]:", peopleData[person])
+
+      fs.writeFile(
+        __dirname + '/peopleData.json',
+        JSON.stringify(peopleData, null, 2),
+        function (err) {
+          if (!err) {
+            res.status(200).send("Photo was successfully stored.")
+          } else {
+            res.status(500).send("Error storing photo in DB.")
+          }
+        }
+      )
+    } else {
+      res.status(400).send("Request body needs `url` and `caption`.")
+    }
+  } else {
+    next()
+  }
+
+})
 
 app.get('*', function (req, res, next) {
   res.status(404).render('404', {
